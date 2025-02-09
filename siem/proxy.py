90c688c3a3d591835809309723e5f070208f42e0
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2025-02-09 20:22:22 -0300

    doc: adding docstring to proxy module

--- siem/proxy.py
+++ siem/proxy.py
@@ -9,8 +9,21 @@ from siem.user import check_create_savedir
 import geopandas as gpd
 
 
-def get_domain_extension(wrf_path: str = "../data/geo_em.d02.nc") -> tuple:
-    geo = xr.open_dataset(wrf_path)
+def get_domain_extension(geo_em_path: str = "../data/geo_em.d02.nc") -> tuple:
+    '''
+    Extract wrf domain corners latitude and longitude.
+
+    Parameters
+    ----------
+    geo_em_path : str
+        Location of geo_em.d0x.nc file
+
+    Returns
+    -------
+    tuple
+        max latitude, min latitude, max longitude, and min longitude.
+    '''
+    geo = xr.open_dataset(geo_em_path)
     xlat_c = geo.XLAT_C.isel(Time=0)
     xlon_c = geo.XLONG_C.isel(Time=0)
     north = xlat_c.max().values
@@ -22,6 +35,22 @@ def get_domain_extension(wrf_path: str = "../data/geo_em.d02.nc") -> tuple:
 
 def get_highway_query(highway_types: list[str],
                       add_links: bool = False) -> str:
+    '''
+    Creates the custiom query based on highways types to use in OSMx package.
+    See <https://wiki.openstreetmap.org/wiki/Key:highway>.
+
+    Parameters
+    ----------
+    highway_types: list[str]
+        List with the types of highways to download.
+    add_links: bool
+        If highways type links need to be download.
+
+    Returns
+    -------
+    str
+        The custom query to use OSMx.
+    '''
     if add_links:
         highway_links = [f"{st}_link" for st in highway_types]
         highway_types += highway_links
@@ -33,13 +62,34 @@ def get_highway_query(highway_types: list[str],
     return cf
 
 
-def download_highways(wrf_path: str, highway_types: str,
+def download_highways(geo_em_path: str, highway_types: str,
                       add_links: bool = False,
                       save: bool = True,
                       save_path: str = "../data/partial",
                       file_name: str = "highway"):
+    '''
+    Download highways types contain in WRF domains
 
-    north, south, east, west = get_domain_extension(wrf_path)
+    Parameters
+    ----------
+    wrf_path: str
+        location of geo_em.d0x file.
+    highway_type: str
+        List with the types of highways to download.
+    add_links: bool
+        If highways type links need to be download.
+    save: bool
+        To save the file.
+    save_bath: str
+        Location to save downloaded highways.
+    file_name:
+        Identifier of save file.
+
+    Returns
+    -------
+    Highways in domain in graph.ml format.
+    '''
+    north, south, east, west = get_domain_extension(geo_em_path)
     custom_filter = get_highway_query(highway_types, add_links)
     highways = ox.graph_from_bbox(north, south, east, west,
                                   network_type="drive",
@@ -51,13 +101,31 @@ def download_highways(wrf_path: str, highway_types: str,
     return highways
 
 
-def download_point_sources(wrf_path: str, tags: dict,
+def download_point_sources(geo_em_path: str, tags: dict,
                            save: bool = True,
                            save_path: str = "../data/partial"):
-    '''Download point sources like:
-        fuel stations tags={"amenity": "fuel"}
-        pizza restaurant tags={"cuisine": "pizza"}'''
-    north, south, east, west = get_domain_extension(wrf_path)
+    '''
+    Download point sources like fuel staions or restaurants.
+    It is based on OSM amenities. This function uses tags that can be found in
+    <https://wiki.openstreetmap.org/wiki/Key:amenity>.
+
+    Parameters
+    ----------
+    wrf_path: str
+        location of geo_em.d0x file.
+    tags: dict
+        For example, fuel stations `tags={"amenity": "fuel"}` or
+        for pizza restaurant `tags={"cuisine": "pizza"}`
+    save: bool
+        To save the file.
+    save_bath: str
+        Location to save downloaded amenities.
+
+    Returns
+    -------
+        Point amenities in shapefile.
+    '''
+    north, south, east, west = get_domain_extension(geo_em_path)
     point_sources = ox.features_from_bbox(north, south, east, west,
                                           tags=tags)
     point_sources_shp = point_sources[["name", "geometry"]].centroid
@@ -73,6 +141,20 @@ def download_point_sources(wrf_path: str, tags: dict,
 
 
 def create_grid(geo_em: xr.Dataset) -> gpd.GeoDataFrame:
+    '''
+    Create grid based of geo_em.d0X.nc file. It is compatible with
+    Mercator and Lambert projections.
+
+    Parameters
+    ----------
+    geo_em: xr.Dataset
+        WRF geo_em.d0x.nc file
+
+    Returns
+    -------
+    gpd.GeoDataFrame
+        WRF domain grid in GeoDataFrame format.
+    '''
     clat = geo_em.XLAT_C.sel(Time=0).values
     clon = geo_em.XLONG_C.sel(Time=0).values
 
@@ -101,9 +183,21 @@ def create_grid(geo_em: xr.Dataset) -> gpd.GeoDataFrame:
     return grid_wrf
 
 
-def create_wrf_grid(geo_path: str, save: bool = True,
+def create_wrf_grid(geo_em_path: str, save: bool = True,
                     save_path: str = "../data/partial") -> gpd.GeoDataFrame:
-    with xr.open_dataset(geo_path) as geo:
+    '''
+    Create WRF domain grid.
+
+    Parameters
+    ----------
+    geo_em_path: str
+        Location of geo_em.d0X.nc file.
+    save: bool
+        If grid needs to be saved.
+    save_path:
+        Location to save wrf grid.
+    '''
+    with xr.open_dataset(geo_em_path) as geo:
         wrf_grid = create_grid(geo)
         grid_id = geo.grid_id
     if save:
@@ -114,6 +208,22 @@ def create_wrf_grid(geo_path: str, save: bool = True,
 
 def configure_grid_spatial(wrf_grid: gpd.GeoDataFrame,
                            proxy: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    '''
+    Add CRS to WRF grid and added ID, preparing grid to be intecepted
+    with highway shp.
+
+    Parameters
+    ----------
+    wrf_grid: gpd.GeoDataFrame
+        WRF domain grid.
+    proxy: gpd.GeoDataFrame
+        proxy shapefile (highways)
+
+    Returns
+    -------
+    gpd.GeoDataFrame
+        wrf_grid with same CRS as proxy and with column ID.
+    '''
     wrf_grid = wrf_grid.set_crs(proxy.crs)
     wrf_grid["ID"] = range(0, len(wrf_grid))
     return wrf_grid
@@ -124,6 +234,27 @@ def calculate_points_grid(wrf_grid: gpd.GeoDataFrame,
                           to_pre: bool = True,
                           save_pre: str = "../data",
                           file_name: str = "my_src") -> gpd.GeoDataFrame:
+    """
+    Calculate number of points (osm amenites) in each wrf grid cells.
+
+    Parameters
+    ----------
+    wrf_grid : gpd.GeoDataFrame
+        WRF domain grid.
+    proxy : gpd.GeoDataFrame
+        proxy shapefile (amenities)
+    to_pre : bool
+        If needed to prepare spatial proxy.
+    save_pre : str
+        Location to save file.
+    file_name : str
+        File identifier.
+
+    Returns
+    -------
+    gpd.GeoDataFrame
+       Number of points in each cell grid.
+    """
     wrf_grid_ready = configure_grid_spatial(wrf_grid,
                                             proxy)
     proxy_in_grid = gpd.clip(proxy, wrf_grid_ready)
@@ -147,7 +278,20 @@ def calculate_points_grid(wrf_grid: gpd.GeoDataFrame,
     return points_in_dom
 
 
-def load_osmx_to_gdfs(osmx_path):
+def load_osmx_to_gdfs(osmx_path: str) -> gpd.GeoDataFrame:
+    """
+    Load osmx highways (graph.ml) to gpd.GeoDataFrame.
+
+    Parameters
+    ----------
+    osmx_path : str
+       Location of highways type data. 
+
+    Returns
+    -------
+    gpd.GeoDataFrame
+        Highways in GeoDataFrame.
+    """
     sp = ox.load_graphml(osmx_path)
     return ox.graph_to_gdfs(sp, nodes=False, edges=True)
 
@@ -157,6 +301,29 @@ def calculate_highway_grid(wrf_grid: gpd.GeoDataFrame,
                            to_pre: bool = True,
                            save_pre: str = "../data/",
                            file_name: str = "my_src") -> gpd.GeoDataFrame:
+    """
+    Calculate sum of highways longitude inside WRF grid cell.
+    This will produce a proxy for spatially distribute vehicular emissions.
+
+    Parameters
+    ----------
+    wrf_grid : gpd.GeoDataFrame
+        WRF domain grid.    
+    proxy : gpd.GeoDataFrame
+        Highways shapefile.
+    to_pre : bool
+        If needed to prepare spatial proxy.
+    save_pre : str
+        Location to save file.
+    file_name : str
+        File identifier name.
+
+    Returns
+    -------
+    gpd.GeoDataFrame
+        Sum of longitudes of all highways type inside grid cell.
+
+    """
     wrf_grid_ready = configure_grid_spatial(wrf_grid, proxy)
 
     highway = proxy[["highway", "length", "geometry"]]
