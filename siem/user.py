766d2ad862477df0c6730a36d66df8e01184554f
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2025-02-10 21:38:13 -0300

    doc: user module docstring added

--- siem/user.py
+++ siem/user.py
@@ -1,3 +1,14 @@
+# siem/user.py
+""" 
+Function for user output
+
+This modules have functions to user.
+
+It allows the following functions:
+    - `check_create_savedir(save_path)` - Create saving directory if does not exists.
+
+"""
+
 import os
 
 def check_create_savedir(save_path: str) -> None:
