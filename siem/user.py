136ad000bc273fe7e412d830d25268c07d7507fa
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2025-02-11 19:58:42 -0300

    fix: unused f-string

--- siem/user.py
+++ siem/user.py
@@ -13,8 +13,8 @@ import os
 
 def check_create_savedir(save_path: str) -> None:
     if not os.path.isdir(save_path):
-        print(f'Missing directory, creating directory')
+        print('Missing directory, creating directory')
         os.makedirs(save_path)
-    print(f'Saving in {save_path}')
+    print('Saving in {save_path}')
     return None
 
