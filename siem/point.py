d64eb64859b146b12a68625085a7acf51bf62b8c
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2025-03-18 19:07:59 -0300

    doc: change to google docstring

--- siem/point.py
+++ siem/point.py
@@ -1,18 +1,16 @@
 # siem/point.py
-"""
-Functions for point sources emissions.
+"""Functions for point sources emissions.
 
 This module allows to work with PointSource class.
 
 It contains the following functions:
-    - `create_gpd_from(point_src_path, sep, lat_name, lon_name)` - Returns point sources csv in gpd.GeoDataFrame.
-    - `calculate_sum_points(point_src, wrf_grid)` - Returns wrf_grid with sums of point sources emissions.
-    - `create_emiss_point(point_src, wrf_grid)` - Returns wrf_grid with sums of point sources emissions and cell with no emissions with 0.
-    - `retrive_proj_from(geogrid_path)` - Returns wrf domain projections.
-    - `calculate_centroids(emiss_point, geo_path)` - Returns total emissions of point sources in wrf grid cell centroids.
-    - `point_emiss_to_xarray(emiss_point_proj)` - Returns emissions in centroids in xr.Dataset.
-    - `read_point_sources(point_path, geo_path, sep, lat_name, lon_name)` - Returns spatial distributed point sources emissions to use PointSources class.
-
+    - `create_gpd_from(point_src_path, sep, lat_name, lon_name)` - Returns: point sources csv in gpd.GeoDataFrame.
+    - `calculate_sum_points(point_src, wrf_grid)` - Returns: wrf_grid with sums of point sources emissions.
+    - `create_emiss_point(point_src, wrf_grid)` - Returns: wrf_grid with sums of point sources emissions and cell with no emissions with 0.
+    - `retrive_proj_from(geogrid_path)` - Returns: wrf domain projections.
+    - `calculate_centroids(emiss_point, geo_path)` - Returns: total emissions of point sources in wrf grid cell centroids.
+    - `point_emiss_to_xarray(emiss_point_proj)` - Returns: emissions in centroids in xr.Dataset.
+    - `read_point_sources(point_path, geo_path, sep, lat_name, lon_name)` - Returns: spatial distributed point sources emissions to use PointSources class.
 """
 import numpy as np
 import pandas as pd
@@ -31,22 +29,14 @@ def create_gpd_from(point_src_path: str, sep: str = "\t",
     Read .csv file with latitude and longitude of point sources,
     each column is the total emissions in KTn/year.
 
-    Parameters
-    ----------
-    point_src_path : str
-        Location of point source .csv file.
-    sep : str
-        Column separator of point_src file.
-    lat_name : str
-        Latitude column name.
-    lon_name : str
-        Longitude column name.
+    Args:
+        point_src_path: Location of point source .csv file.
+        sep: Column separator of point_src file.
+        lat_name: Latitude column name.
+        lon_name: Longitude column name.
 
-    Returns
-    -------
-    gpd.GeoDataFrame
+    Returns:
         Point sources and emissions in GeoDataFrame.
-
     """
     point_sources = pd.read_csv(point_src_path, sep=sep)
     point_sources = gpd.GeoDataFrame(
@@ -61,21 +51,14 @@ def create_gpd_from(point_src_path: str, sep: str = "\t",
 
 def calculate_sum_points(point_src: gpd.GeoDataFrame,
                          wrf_grid: gpd.GeoDataFrame) -> pd.DataFrame:
-    """
-    Calculate sum of emissions from each point sources in a wrf grid cell.
+    """Calculate sum of emissions from each point sources in a wrf grid cell.
 
-    Parameters
-    ----------
-    point_src : gpd.GeoDataFrame
-        GeoDataFrame of point sources. 
-    wrf_grid : gpd.GeoDataFrame
-        WRF domain grid
+    Args:
+        point_src: GeoDataFrame of point sources. 
+        wrf_grid: WRF domain grid
 
-    Returns
-    -------
-    pd.DataFrame
+    Returns:
         Sum of emission in each grid.
-
     """
     point_in_grid = gpd.overlay(point_src, wrf_grid, how="intersection")
     points_in_grid = point_in_grid.dissolve("ID", aggfunc="sum")
@@ -89,36 +72,25 @@ def create_emiss_point(point_src: gpd.GeoDataFrame,
     Complete wrf grid cell with no point sources emissions with 0.
     TODO: change name of this function.
 
-    Parameters
-    ----------
-    point_src : gpd.GeoDataFrame
-        GeoDataFrame of point sources.
-    wrf_grid : gpd.GeoDataFrame
-        WRF domain grid
+    Args:
+        point_src: GeoDataFrame of point sources.
+        wrf_grid: WRF domain grid
 
-    Returns
-    -------
-    gpd.GeoDataFrame
+    Returns:
         Complete grid with total emission in wrf domain grid.
-
     """
     points_in_grid = calculate_sum_points(point_src, wrf_grid)
     return wrf_grid.join(points_in_grid).fillna(0)
 
 
 def retrive_proj_from(geogrid_path: str):
-    """
-    Extract projection of geo_em.d0x.nc file.
+    """ Extract projection of geo_em.d0x.nc file.
 
-    Parameters
-    ----------
-    geogrid_path : str
-       Location of geo_em.d0x.nc file.
+    Args:
+        geogrid_path: Location of geo_em.d0x.nc file.
 
-    Returns
-    -------
+    Returns:
         Projection of geo_em.d0x.nc
-        
     """
     with Dataset(geogrid_path) as geo_dom:
         hgt = getvar(geo_dom, "HGT_M")
@@ -127,21 +99,14 @@ def retrive_proj_from(geogrid_path: str):
 
 def calculate_centroid(emiss_point: gpd.GeoDataFrame,
                        geo_path: str) -> pd.DataFrame:
-    """
-    Calculate centroid of each WRF domain grid cell.
+    """Calculate centroid of each WRF domain grid cell.
 
-    Parameters
-    ----------
-    emiss_point : gpd.GeoDataFrame
-        Total emissions in each wrf domain cell.    
-    geo_path : str
-        Location of geo_em.d0x.nc file.
+    Args:
+        emiss_point: Total emissions in each wrf domain cell.    
+        geo_path : Location of geo_em.d0x.nc file.
 
-    Returns
-    -------
-    pd.DataFrame
+    Returns:
        A DataFrame with total emissions from point sources with cell centroid coordinates. 
-
     """
     wrf_proj = retrive_proj_from(geo_path)
     emiss_point_proj = emiss_point.to_crs(wrf_proj)
@@ -153,19 +118,13 @@ def calculate_centroid(emiss_point: gpd.GeoDataFrame,
 
 
 def point_emiss_to_xarray(emiss_point_proj: pd.DataFrame) -> xr.Dataset:
-    """
-    Transform point sources dataframe into a xr.Dataset.
+    """Transform point sources dataframe into a xr.Dataset.
 
-    Parameters
-    ----------
-    emiss_point_proj : pd.DataFrame
-        Total emissions in cell grid with centroid.
+    Args:
+        emiss_point_proj: Total emissions in cell grid with centroid.
 
-    Returns
-    -------
-    xr.Dataset
+    Returns:
         Total emission in xr.Dataset.
-
     """
     lon1d = emiss_point_proj.x.round(5).unique()
     lat1d = emiss_point_proj.y.round(5).unique()
@@ -190,27 +149,17 @@ def point_emiss_to_xarray(emiss_point_proj: pd.DataFrame) -> xr.Dataset:
 def read_point_sources(point_path: str, geo_path: str, sep: str = "\t",
                        lat_name: str = "LAT", lon_name: str = "LON"
                        ) -> xr.Dataset:
-    """
-    Read point sources .csv file to produces a xr.Dataset.
+    """Read point sources .csv file to produces a xr.Dataset.
 
-    Parameters
-    ----------
-    point_path : str
-        Location of point sources .csv file.
-    geo_path : str
-        Location of geo_em.d0X.nc file.
-    sep : str
-        Column separator of point sources .csv file.
-    lat_name : str
-        Latitude column name.
-    lon_name : str
-        Longitude column name.
+    Args:
+        point_path: Location of point sources .csv file.
+        geo_path: Location of geo_em.d0X.nc file.
+        sep: Column separator of point sources .csv file.
+        lat_name: Latitude column name.
+        lon_name: Longitude column name.
 
-    Returns
-    -------
-    xr.Dataset
+    Returns:
         Total emissions of point sources in each WRF domain cell xr.Dataset.
-
     """
     point_sources = create_gpd_from(point_path, sep, lat_name, lon_name)
     wrf_grid = create_wrf_grid(geo_path, save=False)
