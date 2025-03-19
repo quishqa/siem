054f319a8d8a682b96fcae16b9947d84f1921121
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2025-03-19 11:55:54 -0300

    doc: change to google docstring

--- siem/spatial.py
+++ siem/spatial.py
@@ -1,16 +1,14 @@
 # siem/spatial.py
-"""
-Functions for emission spatial disaggregation.
+"""Functions for emission spatial disaggregation.
 
 This modules helps you to read proxies to spatially distribute emissions, 
 and to perform the spatial emission distribution.
 
 It contains the following functions:
 
-    - `read_spatial_proxy(proxy_path, proxy_shape, col_names, sep, proxy, lon_name, lat_name)` - Returns spatial proxy (weight) in xr.DataArray.
-    - `calculate_density_map(spatial_proxy, number_sources, cell_area)` - Returns number of emissions by km^2.
+    - `read_spatial_proxy(proxy_path, proxy_shape, col_names, sep, proxy, lon_name, lat_name)` - Returns: spatial proxy (weight) in xr.DataArray.
+    - `calculate_density_map(spatial_proxy, number_sources, cell_area)` - Returns: number of emissions by km^2.
     - `distribute_spatial_emission(spatial_proxy, number_sources, cell_area, use_intensity, pol_ef, pol_name)` - Calculate total emissions of a pollutant (g day^-1 km^-2)
-
 """
 import pandas as pd
 import xarray as xr
@@ -29,28 +27,17 @@ def read_spatial_proxy(proxy_path: str,
     Read spatial proxy (emission weights) csv file.
     It has to have the same number of points as wrfinput file.
 
-    Parameters
-    ----------
-    proxy_path : str
-        The location of the csv file.
-    proxy_shape: tuple(nrow, ncol)
-        Dimensions of proxy, number of columns and number of rows.
-    col_names : list
-        Columns name of the csv file.
-    sep : str
-        csv file separator.
-    proxy : str
-        The column with the proxy value.
-    lon_name : str
-        Column name of the longitude.
-    lat_name : str
-        Column name of the latitude.
+    Args:
+        proxy_path: The location of the csv file.
+        proxy_shap: Dimensions of proxy, number of columns and number of rows.
+        col_names: Columns name of the csv file.
+        sep: csv file separator.
+        proxy: The column with the proxy value.
+        lon_name: Column name of the longitude.
+        lat_name: Column name of the latitude.
 
-    Returns
-    -------
-    xr.DataArray
+    Returns:
         Spatial proxy with dimensions as wrfinput.
-
     """
     spatial_proxy = pd.read_csv(proxy_path, names=col_names, sep=sep)
     ncol, nrow = proxy_shape
@@ -84,20 +71,13 @@ def calculate_density_map(spatial_proxy: xr.DataArray,
     Transform the proxy (emission weight) into a density of emission
     sources (# emission sources / km ^2).
 
-    Parameters
-    ----------
-    spatial_proxy : xr.DataArray
-        Spatial emission weights.
-    number_sources : int | float
-        Number of sources in the domain.
-    cell_area : int | float
-        wrfinput cell area (km^2)
+    Args:
+        spatial_proxy: Spatial emission weights.
+        number_sources: Number of sources in the domain.
+        cell_area: wrfinput cell area (km^2)
 
-    Returns
-    -------
-    xr.DataArray
+    Returns:
         Spatial distribution of number of sources by square km^2.
-
     """
     total_proxy = spatial_proxy.sum()
     ratio = number_sources / total_proxy
@@ -110,29 +90,18 @@ def distribute_spatial_emission(spatial_proxy: xr.DataArray,
                                 use_intensity: float,
                                 pol_ef: float,
                                 pol_name: str) -> xr.DataArray:
-    """
-    Calculate the total emission of a pollutant in each cell.
+    """Calculate the total emission of a pollutant in each cell.
 
-    Parameters
-    ----------
-    spatial_proxy : xr.DataArray
-        Spatial emission weights.
-    number_sources : int | float
-        Number of sources in the domain.
-    cell_area : float
-        wrfinput cell area (km^2)
-    use_intensity : float
-        Emission source use intensity (km/day).
-    pol_ef : float
-        Pollutant emission factor (g/km).
-    pol_name : str
-        Pollutant name.
+    Args:
+        spatial_proxy: Spatial emission weights.
+        number_sources: Number of sources in the domain.
+        cell_area: wrfinput cell area (km^2)
+        use_intensity: Emission source use intensity (km/day).
+        pol_ef: Pollutant emission factor (g/km).
+        pol_name: Pollutant name.
 
-    Returns
-    -------
-    xr.DataArray
+    Returns:
         Emission of a single pollutant (g/day).
-        
     """
     # TODO: move to emiss.py
     density_map = calculate_density_map(spatial_proxy,
