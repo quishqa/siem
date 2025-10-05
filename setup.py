2cf84457daee79417bfcf68e08a9a35a60853089
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2025-10-05 20:14:53 -0300

    feat: additional meta data

--- setup.py
+++ setup.py
@@ -6,11 +6,19 @@ with open("README.md", "r") as f:
 setup(
     name="siem",
     version="0.0.1",
-    description="A package to build your emission files",
+    description="A package to build WRF-Chem and CMAQ emission files",
     author="Mario Gavidia-Calderón",
     author_email="mario.calderon@iag.usp.br",
     packages=find_packages(),
     setup_requires=["numpy"],
     install_requires=["numpy"],
     long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.10",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
 )
